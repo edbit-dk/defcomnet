5052bf71e6031e3f70c360693aeb82f53bcecaba
Thomas Elvin <thomas.e.s.l@outlook.dk>
2026-02-25 22:44:07 +0100

    text hosts
    new hosts

--- database/migrations/HostTable.php
+++ database/migrations/HostTable.php
@@ -23,7 +23,7 @@ class HostTable extends Host
             $table->string('password')->nullable();
             $table->text('welcome')->nullable();
             $table->string('org')->nullable();
-            $table->enum('os', ['U/DOS V1.10', 'U/DOS V3.0', 'U/DOS V4.0', 'CAX/MUDOS V1.4', '[REDACTED]'])->default('U/DOS V1.10');
+            $table->enum('os', ['U/DOS V1.10', 'U/DOS V3.0', 'U/DOS V4.0', 'MU/DOS V1.4', '4.3 BSD VAX-11/780', '[REDACTED]'])->default('U/DOS V1.10');
             $table->string('location')->nullable();
             $table->ipAddress('ip')->unique();
             $table->decimal('ip_num', 39,0)->unsigned()->default(0);
